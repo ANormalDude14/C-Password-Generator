06f57c33940ba1e727694271ecf33be7c86a5d3d
Jr.Timmothy <112587120+ANormalDude14@users.noreply.github.com>
2022-10-09 16:28:31 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Program
+{
+    class NumberGen
+    {
+        static void Main(String[] args)
+        {   
+         CalcArgue();
+        }
+        static void CalcArgue()
+        {
+           Random rnd = new Random();
+          
+          string _Ask = "Many: ";
+          Console.WriteLine(_Ask);
+
+          int RandPass = Convert.ToInt32(Console.ReadLine());
+
+          for (int J = 0; J < RandPass; J++)
+          {
+             Console.WriteLine(rnd.Next());
+          }
+        }
+    }
+}
